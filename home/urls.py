ff856b2b4380421e5fde16d3f1cd810b97a2f1c8
Josh Bright <josh.bright@gmail.com>
2021-07-22 20:49:00 -0700

    Changed the project link on the main page to link to a subpage that describes the project

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='home-index'),
+    path("", views.index, name="home-index"),
+    path("project/<str:slug>/", views.project, name="home-project"),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "home/index.html", {})
+
+
+def project(request, slug):
+    template_name = "home/{}.html".format(slug)
+    return render(request, template_name, {})
