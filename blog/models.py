a041722c824c2a0645d779d6fa7f21af995c3cec
Josh Bright <josh.bright@gmail.com>
2021-07-29 18:38:58 -0700

    Added blog post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=128, unique=True)
+    slug = models.SlugField(null=False, unique=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    content = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
