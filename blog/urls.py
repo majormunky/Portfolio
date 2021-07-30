15b86269271a58fca772c398da42fcd55bb4b320
Josh Bright <josh.bright@gmail.com>
2021-07-29 18:32:26 -0700

    Added page that we will use to create a blog post

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from blog import views
 
 urlpatterns = [
     path("", views.index, name="blog-index"),
+    path("create/", views.create_blog_post, name="blog-create-post"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     return render(request, "blog/index.html", {})
+
+
+@login_required
+def create_blog_post(request):
+    return render(request, "blog/create-blog-post.html", {})
