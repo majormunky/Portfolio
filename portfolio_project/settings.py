7e1a1d2186321086e0ebaa6dcdd41399cfdf3d3d
Josh Bright <josh.bright@gmail.com>
2021-07-29 18:04:37 -0700

    Added localhost and local address to allowed sites

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DJANGO_DEBUG"] == "True"
 
-ALLOWED_HOSTS = ["joshbright.com"]
+ALLOWED_HOSTS = ["joshbright.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "home",
     "core",
+    "blog",
 ]
 
 MIDDLEWARE = [
