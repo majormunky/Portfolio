3bddd7ace0df7454eeb7f2992b6fd4d1c8efed94
Josh Bright <josh.bright@gmail.com>
2021-07-29 17:38:00 -0700

    Added domain to allowed hosts setting

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DJANGO_DEBUG"] == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["joshbright.com"]
 
 
 # Application definition
