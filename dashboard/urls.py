822fc32a912bb93bf80ce8abff5c9093fcdc2370
Josh Bright <josh.bright@gmail.com>
2021-11-22 20:15:41 -0800

    Added dashboard app

---
+++ dashboard/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from dashboard import views
+
+
+urlpatterns = [
+    path("", views.index, name="dashboard-index"),
+]
+
--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("dashboard/", include("dashboard.urls")),
     path("", include("home.urls")),
     path("blog/", include("blog.urls")),
 ]
