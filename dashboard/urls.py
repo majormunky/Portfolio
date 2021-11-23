6a6180714ce7b594ced82a69bd8cb8a7cc5dfd33
Josh Bright <josh.bright@gmail.com>
2021-11-22 22:10:44 -0800

    Added an edit-post page.  We can now bring up a post and have the formatting saved within the quill editor, but, we cannot save yet

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="dashboard-index"),
     path("posts/", views.posts, name="dashboard-posts"),
     path("posts/create/", views.create_post, name="dashboard-create-post"),
+    path('posts/<int:pk>/edit/', views.edit_post, name="dashboard-edit-post"),
 ]
 
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from blog import models as blog_models
 
@@ -18,7 +18,7 @@ def create_post(request):
         content = request.POST.get("content", None)
         if not all([title, content]):
             return JsonResponse({"result": "failed"})
-
+        
         new_post = blog_models.BlogPost(
             title=title,
             content=content,
@@ -30,3 +30,7 @@ def create_post(request):
         return render(request, "dashboard/create-post.html", {})
 
 
+def edit_post(request, pk):
+    blog_data = get_object_or_404(blog_models.BlogPost, pk=pk)
+    blog_dict = {"title": blog_data.title, "content": blog_data.content}
+    return render(request, "dashboard/edit-post.html", {"blog_data": blog_dict})
