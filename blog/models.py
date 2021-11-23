adcdbe80df6788baa08286694cec3d24c2c5bef9
Josh Bright <josh.bright@gmail.com>
2021-11-22 19:09:50 -0800

    Added a method on the blog post model to return its url

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth import get_user_model
+from django.utils.text import slugify
 
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=128, unique=True)
-    slug = models.SlugField(null=False, unique=True)
+    slug = models.SlugField(blank=True, null=False, unique=True)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     content = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(default=timezone.now)
@@ -14,3 +15,6 @@ class BlogPost(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         return super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return "{}/{}/{}/".format(self.created_at.year, str(self.created_at.month).zfill(0), self.slug)
