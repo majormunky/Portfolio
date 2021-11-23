ca4b2356aabeae883cb1aadc00539d4261331986
Josh Bright <josh.bright@gmail.com>
2021-11-22 19:11:32 -0800

    Updated the blog list page to return a real list of blog posts instead of it being a dummy list

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from . import models
 
 
 def index(request):
-    return render(request, "blog/index.html", {})
+    blog_list = models.BlogPost.objects.all().order_by("created_at")
+    return render(request, "blog/index.html", {"blog_list": blog_list})
+
+
+def blog_detail(request, year, month, slug):
+    pass
 
 
 @login_required
