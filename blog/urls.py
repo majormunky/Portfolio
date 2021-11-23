5c2b709f1ddab80755afbb52dc7767170e5d3216
Josh Bright <josh.bright@gmail.com>
2021-11-22 19:11:40 -0800

    Added a blog detail page

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from blog import views
 urlpatterns = [
     path("", views.index, name="blog-index"),
     path("create/", views.create_blog_post, name="blog-create-post"),
+    path("<str:year>/<str:month>/<slug:slug>/", views.blog_detail, name="blog-detail"),
 ]
