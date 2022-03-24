6c07e09bb14e1d322f13166360cd18ebd524abc9
Josh Bright <josh.bright@gmail.com>
2022-03-23 20:01:34 -0700

    Added whitenoise and added it to settings

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -138,8 +139,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-# STATICFILES_DIRS = ((BASE_DIR / "static"),)
-STATIC_ROOT = BASE_DIR / "static"
+
+if DEBUG:
+    STATICFILES_DIRS = ((BASE_DIR / "static"),)
+else:
+    STATIC_ROOT = BASE_DIR / "static"
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
