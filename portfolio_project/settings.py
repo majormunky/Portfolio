39a013e7896f3e52c2abe455c6d469acceed2970
Josh Bright <josh.bright@gmail.com>
2022-03-23 19:40:13 -0700

    Added a way to insert an allowed host from environment variables

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -28,7 +28,15 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DJANGO_DEBUG"] == "True"
 
-ALLOWED_HOSTS = ["joshbright.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [
+    "joshbright.com",
+    "localhost",
+    "127.0.0.1",
+]
+
+render_url = os.environ.get("DJANGO_RENDER_URL", None)
+if render_url:
+    ALLOWED_HOSTS.append(render_url)
 
 
 # Application definition
