47876a3b431ecba18e9afca17775be331e8e4e0c
Josh Bright <josh.bright@gmail.com>
2022-03-23 20:40:23 -0700

    Removed whitenoise

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -57,7 +57,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -144,7 +143,6 @@ if DEBUG:
     STATICFILES_DIRS = ((BASE_DIR / "static"),)
 else:
     STATIC_ROOT = BASE_DIR / "static"
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
