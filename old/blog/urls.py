687d947d43e05c22fd7cc59c1da43641e3889696
Josh Bright <josh.bright@gmail.com>
2022-05-07 08:38:29 -0700

    Removed django project

--- old/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from blog import views
-
-urlpatterns = [
-    path("", views.index, name="blog-index"),
-    path("create/", views.create_blog_post, name="blog-create-post"),
-    path("<str:year>/<str:month>/<slug:slug>/", views.blog_detail, name="blog-detail"),
-]
--- old/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path("", views.index, name="home-index"),
-    path("project/<str:slug>/", views.project, name="home-project"),
-    path("resume/", views.resume, name="home-resume"),
-]
